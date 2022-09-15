f7479255132081cde23930e5fe267f45322c3c9a
Alex <Yulien.ru@Yandex.ru>
2022-09-15 21:54:45 +0400

    заменяет строки на столбцы

---
+++ task55/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// В итоге получается вот такой массив:
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+
+int rows = 3;
+int columns = 4;
+
+int [,] GetArray(int m, int n){
+    var result = new int [m, n];
+
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            result[i, j] = new Random().Next(100);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] arr){
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j =0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void ChangeRows(int[,] arr){
+    int rowCount = arr.GetLength(0) - 1;
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        int temp = arr[0, i];
+        arr [0, i] = arr[rowCount, i];
+        arr[rowCount, i] = temp;
+    }
+    PrintArray(arr);
+}
+
+int[,] ChangeArray(int[,] array)
+{   int roes = array.GetLength(0);
+int columns = array.GetLength(1);
+    var result = new int[array.GetLength(1), array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result[j, i] = array[i, j];
+        }
+    }
+    return result;
+}
+
+var arr = GetArray(rows, columns);
+PrintArray(arr);
+System.Console.WriteLine();
+PrintArray(ChangeArray(arr));
